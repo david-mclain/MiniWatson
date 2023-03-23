38c8377f177583941ad5d540f89c379c6e66e5df
david-mclain <davidmclain98@gmail.com>
2023-03-23 12:25:58 -0700

    Created some classes for initial coding

---
+++ src/main/java/Indexer.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.ByteBuffersDirectory;
+public class Indexer {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Error! Missing command line argument.");
+            System.err.println("Usage: java Indexer.java {fileName}");
+        }
+    }
+    
+    private void readData(String fileName) {
+                
+    }
+
+    private void default(String fileName) {
+        // READ UNTIL [[
+
+    }
+}
