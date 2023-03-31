5929e20eeeef3d20362567076020e3a3d123ecaf
david-mclain <davidmclain98@gmail.com>
2023-03-30 17:16:33 -0700

    Set up maven test

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,1 +1,12 @@
-/* Placeholder to push directory structure to repository. */
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Test1 {
+    @Test
+    public void testDocs() {
+        Indexer.main(null);
+    }
+}
