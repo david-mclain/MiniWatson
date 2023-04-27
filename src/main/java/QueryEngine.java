04307d5a70835f4bdc608f5a70d2144db8e0d062
david-mclain <davidmclain98@gmail.com>
2023-04-27 15:07:26 -0700

    Minor tweaks

--- src/main/java/QueryEngine.java
+++ src/main/java/QueryEngine.java
@@ -59,7 +59,6 @@ public class QueryEngine {
     }
 
     QueryEngine(String searchType, String scoringMethod) throws IOException, ParseException {
-        System.out.println("Constructing");
         directoryPath = "src/main/resources/";
         if (searchType.equals("lemma")) {
             directoryPath = directoryPath + "lemmatized-indexed-documents";
@@ -133,7 +132,7 @@ public class QueryEngine {
             int hitsPerPage = 10;
             TopDocs docs = searcher.search(query, hitsPerPage);
             ScoreDoc[] hits = docs.scoreDocs;
-            System.out.println("Currently searching for: " + answer.toLowerCase());
+            System.out.println("Currently searching for: " + answer);
             for(int i = 0; i < hits.length; ++i) {
                 int docId = hits[i].doc;
                 Document d = searcher.doc(docId);
